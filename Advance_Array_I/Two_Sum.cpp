3d23e654f26a09cd13f51cc6f0e63ba224975544
khushi8825 <khushisingh051211@gmail.com>
2025-11-26 20:50:16 +0530

    Advance Array

---
+++ Advance_Array_I/Two_Sum.cpp
@@ -0,0 +1,55 @@
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+TC = O(n^2)
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+
+                ans.push_back(i);
+                ans.push_back(j);
+                break;
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        arr[i] = x;
+    }
+    int target;
+    cin >> target;
+
+    vector<int> ans = twoSum(arr, target);
+    for (int c : ans)
+    {
+        cout << c << " ";
+    }
+    return 0;
+}
