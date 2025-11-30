90e0800ae88b6fa6acefda44ac50b0999cf01065
khushi8825 <khushisingh051211@gmail.com>
2025-11-30 23:03:51 +0530

    Hashmap done

---
+++ Hashmap sliding window/count_of_0_sum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+int countZeroSumSubarrays(vector<int>& arr) {
+    int n = arr.size();
+    unordered_map<int, int> prefixCount;
+    int prefixSum = 0;
+    int count = 0;
+
+    prefixCount[0] = 1;
+
+    for (int i = 0; i < n; i++) {
+        prefixSum += arr[i];
+        if (prefixCount.find(prefixSum) != prefixCount.end()) {
+            count += prefixCount[prefixSum];
+        }
+        prefixCount[prefixSum]++;
+    }
+
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    cout << countZeroSumSubarrays(arr);
+    return 0;
+}
