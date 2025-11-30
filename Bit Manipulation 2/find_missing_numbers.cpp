5f3cd218b072aab0c60cc4f15d3e04b4cca8f6b0
khushi8825 <khushisingh051211@gmail.com>
2025-11-30 23:10:48 +0530

    Bit Manipulation Done

---
+++ Bit Manipulation 2/find_missing_numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int missingNumber(vector<int>& nums) {
+    int n = nums.size();
+    int ans = n;
+    for (int i = 0; i < n; i++)
+        ans ^= i ^ nums[i];
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+    cout << missingNumber(nums) << endl;
+    return 0;
+}
