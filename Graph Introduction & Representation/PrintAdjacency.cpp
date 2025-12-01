a479295c327ddac2d004b34d785c6fad1a99142a
khushi8825 <khushisingh051211@gmail.com>
2025-12-01 20:24:00 +0530

    Graph Done

---
+++ Graph Introduction & Representation/PrintAdjacency.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int V, E;
+    cin >> V >> E;
+
+    vector<vector<int>> adj(V);
+
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    for (int i = 0; i < V; i++) {
+        cout << i << ": ";
+        for (int x : adj[i]) cout << x << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ Graph Introduction & Representation/graph_intro_n_rep2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void addEdge(vector<vector<int>>& adj, int u, int v) {
+    adj[u][v] = 1;
+    adj[v][u] = 1;
+}
+
+int main() {
+    int V = 5;
+
+    vector<vector<int>> adj(V, vector<int>(V, 0));
+
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 4);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 1, 4);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 4);
+
+    cout << "ADJACENCY MATRIX REPRESENTATION\n";
+    cout << "---------------------------------\n\n";
+
+    cout << "    ";
+    for (int j = 0; j < V; ++j)
+        cout << j << " ";
+    cout << "\n";
+
+    cout << "    ";
+    for (int j = 0; j < V; ++j)
+        cout << "--";
+    cout << "\n";
+
+    for (int i = 0; i < V; ++i) {
+        cout << i << " | ";
+        for (int j = 0; j < V; ++j)
+            cout << adj[i][j] << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
